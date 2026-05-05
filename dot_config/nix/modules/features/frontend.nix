8e8f2f3e616c00cbaa7e2abf5adb81b05b33bcea
Artur Iaroshenko <artawower@protonmail.com>
2026-05-05 15:45:11 +0400

    chore(nix): move tool packages into modules

--- dot_config/nix/modules/features/frontend.nix
+++ dot_config/nix/modules/features/frontend.nix
@@ -1,12 +1,18 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-let cfg = config.conf.features.frontend; in
+let
+  cfg = config.conf.features.frontend;
+in
 
 {
   config = lib.mkIf cfg.enable {
     conf.packages.nix = with pkgs; [
       lua-language-server
-      nodePackages.typescript
       google-java-format
     ];
   };
