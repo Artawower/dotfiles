7f6c8a8133acca2bc60cb5a6cd91958e6ddb00fc
Artur Iaroshenko <artawower@protonmail.com>
2026-05-06 02:10:33 +0400

    refactor: remove sketchybar, update karabiner formatting

--- dot_config/sketchybar/items/btc.sh
+++
@@ -1,4 +0,0 @@
-sketchybar -m --add item btc right \
-              --set btc icon= \
-              --set btc update_freq=20 \
-              --set btc script="~/.config/sketchybar/plugins/btc.sh"
